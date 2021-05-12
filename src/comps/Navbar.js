615334602f9b899750185d64b49df016b9ee07c1
”sovsianykov” <swebproger@gmail.com>
2021-05-12 16:49:16 +0300

    three2

--- src/comps/Navbar.js
+++ src/comps/Navbar.js
@@ -1,14 +1,24 @@
 import React from 'react';
-import {AppBar, Typography, TypographyVariant} from "@material-ui/core";
+import {AppBar, Typography, Button, Link, ListItem} from "@material-ui/core";
+import './styles.scss'
 
 function Navbar() {
     return (
         <>
-            <AppBar style={{backgroundColor: 'white'}} className='nav' >
-                <Typography variant='h3' color='initial' className='nav-title' >
-                   портфолио
-                </Typography>
-            </AppBar>
+             <AppBar style={{backgroundColor: 'white'}} className='nav'  >
+
+
+                 <Typography variant='h3' color='initial' className='nav_title' align='left'  >
+                    Мария Овсяникова художник-иллюстратор
+                 </Typography>
+                 {/*<ul className="list">*/}
+                 {/*    <ListItem button component={Link} className='links'>главная</ListItem>*/}
+                 {/*    <ListItem button component={Link} className='links'>обо мне</ListItem>*/}
+                 {/*    <ListItem button component={Link} className='links'>галлерея</ListItem>*/}
+                 {/*    <ListItem button component={Link} className='links'>контакты</ListItem>*/}
+                 {/*</ul>*/}
+
+             </AppBar>
         </>
     );
 }
